b7668e5d168b8a8a23908d65e18fb0aa18539210
Teemu Salonen <teemu.t.salonen@tuni.fi>
2024-05-20 17:55:57 +0300

    Added project files and updated README

---
+++ Models/Riddle.cs
@@ -0,0 +1,14 @@
+﻿namespace RiddlesWebApp.Models
+{
+    public class Riddle
+    {
+        public int Id { get; set; }
+        public string RiddleQuestion { get; set; }
+        public string RiddleAnswer { get; set; }
+
+        public Riddle()
+        {
+
+        }
+    }
+}
